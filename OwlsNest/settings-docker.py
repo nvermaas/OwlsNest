e205e35f8d0a633457184f325680f6ff0e7578e7
Nico Vermaas <vermaas@astron.nl>
2019-12-21 22:16:41 +0100

    adding whitenoise

--- OwlsNest/settings-docker.py
+++ OwlsNest/settings-docker.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
