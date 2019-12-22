e4de7c025d22bac05ae605be9dec9a7332b4e75f
Nico Vermaas <vermaas@astron.nl>
2019-12-22 11:21:45 +0100

    adding docker configuration

--- OwlsNest/urls.py
+++ OwlsNest/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^hiking/admin/', admin.site.urls),
     url(r'^hiking/', include('hiking.urls')),
 ]
 
