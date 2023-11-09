612a1ca0ffb53f7334f79b1a63b46d99f19cb67d
Vermaas <vermaas@astron.nl>
2023-11-09 13:05:27 +0100

    update to Django 4

--- OwlsNest/urls.py
+++ OwlsNest/urls.py
@@ -1,31 +1,17 @@
-"""OwlsNest URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import path,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^hiking_nico/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^hiking_nico/admin/', admin.site.urls),
-    url(r'^hiking_nico/', include('hiking.urls')),
-    url(r'^my_hiking/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^my_hiking/admin/', admin.site.urls),
-    url(r'^my_hiking/', include('hiking.urls')),
+    path('hiking_nico/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('hiking_nico/admin/', admin.site.urls),
+    path('hiking_nico/', include('hiking.urls')),
+    path('my_hiking/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('my_hiking/admin/', admin.site.urls),
+    path('my_hiking/', include('hiking.urls')),
 ]
 
 # in debug mode the url where media is stored points at a local directory in the project
