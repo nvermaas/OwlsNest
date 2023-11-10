5d47266bf4798e3095ec00dcc7f5f555a3f706d9
Vermaas <vermaas@astron.nl>
2023-11-10 08:31:37 +0100

    debug true for troubleshooting on AWS

--- OwlsNest/settings-docker.py
+++ OwlsNest/settings-docker.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!sls2(u-2^+*(ob%hc5xrifx0i#ekgguj%c3i_k@ia001w$it0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
