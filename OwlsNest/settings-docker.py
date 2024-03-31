8f5d33874c6a4588a99dafda2843d9c539cce381
Vermaas <vermaas@astron.nl>
2024-03-31 12:30:45 +0200

    fiddling with some settings

--- OwlsNest/settings-docker.py
+++ OwlsNest/settings-docker.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
 #	'webpack_loader'
 ]
 
@@ -137,7 +138,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'PAGE_SIZE': 100
 }
 
