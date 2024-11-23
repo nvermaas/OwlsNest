18be388bb2ac0de6253adb408523a6247aeed500
Vermaas <vermaas@astron.nl>
2024-11-23 11:39:40 +0100

    move static back to /static_hiking/

--- OwlsNest/settings/base.py
+++ OwlsNest/settings/base.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-#STATIC_URL = '/static_hiking/'
-STATIC_URL = FORCE_SCRIPT_NAME + 'hiking/static/'
+STATIC_URL = '/static_hiking/'
+#STATIC_URL = FORCE_SCRIPT_NAME + 'hiking/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
