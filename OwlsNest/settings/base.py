d027d58903851678042aa16e2f7e3f1761a0e0e1
Vermaas <vermaas@astron.nl>
2024-11-23 11:49:14 +0100

    use 'Owlsnest/static'

--- OwlsNest/settings/base.py
+++ OwlsNest/settings/base.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 #STATIC_URL = '/static_hiking/'
-STATIC_URL = FORCE_SCRIPT_NAME + 'hiking/static/'
+STATIC_URL = FORCE_SCRIPT_NAME + 'Owlsnest/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
