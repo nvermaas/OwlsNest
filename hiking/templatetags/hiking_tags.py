3a90e59b8afb364760f1be9536a7668528546097
Vermaas <vermaas@astron.nl>
2025-11-29 20:08:40 +0100

    add overnight tags

--- hiking/templatetags/hiking_tags.py
+++ hiking/templatetags/hiking_tags.py
@@ -8,14 +8,17 @@ def overnights(hike):
 
     wild_rough_nights_html = f"<i class='fas fa-campground' style='color: red;'></i> {hike.wild_rough_nights} " \
         if hike.wild_rough_nights else ""
-    wild_campsite_nights_html = f"<i class='fas fa-campground' style='color: green;'></i> {hike.wild_campsite_nights} " \
+    wild_campsite_nights_html = f"<i class='fas fa-campground' style='color: orange;'></i> {hike.wild_campsite_nights} " \
         if hike.wild_campsite_nights else ""
-    campground_nights_html = f"<i class='fas fa-campground' style='color: blue;'></i> {hike.campground_nights} " \
+    campground_nights_html = f"<i class='fas fa-campground' style='color: green;'></i> {hike.campground_nights} " \
         if hike.campground_nights else ""
     indoor_nights_html = f"<i class='fa-solid fa-house' style='color: green;'></i> {hike.indoor_nights} " \
         if hike.indoor_nights else ""
 
-    overnights_html = wild_rough_nights_html + wild_campsite_nights_html + campground_nights_html + indoor_nights_html
+
+    meta = f"<b>{hike.kilometers}</b> km in <b>{hike.days}</b> days. " if hike.kilometers else ""
+
+    overnights_html = meta + wild_rough_nights_html + wild_campsite_nights_html + campground_nights_html + indoor_nights_html
     return mark_safe(overnights_html)
 
 @register.simple_tag
